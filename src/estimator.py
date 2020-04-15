6d076fe08a5fc52b4feb48e4814529df154a4f3a
Ajawu David <ajawudavid@gmail.com>
2020-04-15 12:26:44 +0100

    modified backend api link to include endpoint

--- src/estimator.py
+++ src/estimator.py
@@ -72,17 +72,17 @@ def estimator(data):
 
     return {'data': data, 'impact': impact, 'severeImpact': severeImpact}
 
-#
-# print(estimator({
-#     "region": {
-#         "name": "Africa",
-#         "avgAge": 19.7,
-#         "avgDailyIncomeInUSD": 4,
-#         "avgDailyIncomePopulation": 0.73
-#     },
-#     "periodType": "days",
-#     "timeToElapse": 38,
-#     "reportedCases": 2747,
-#     "population": 92931687,
-#     "totalHospitalBeds": 678874
-# }))
+
+print(estimator({
+    "region": {
+        "name": "Africa",
+        "avgAge": 19.7,
+        "avgDailyIncomeInUSD": 4,
+        "avgDailyIncomePopulation": 0.73
+    },
+    "periodType": "days",
+    "timeToElapse": 38,
+    "reportedCases": 2747,
+    "population": 92931687,
+    "totalHospitalBeds": 678874
+}))
